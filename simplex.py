859928f7da4da37369323d0eb67a7f6875006a27
SirJowi <johann-sinner@web.de>
2022-12-09 20:12:41 +0100

    fixed normierung

--- simplex.py
+++ simplex.py
@@ -18,6 +18,7 @@ c = np.array([2.0, 3.0, 0.0, 0.0, 0.0])
 # Platzhalter für Hilfsgröße q (Liste mit Nullen und der gleichen Zeilenanzahl wie A)
 q = np.empty(len(A))
 
+# Ergebnis der Optimierungsfunktion
 z = 0.0
 
 # Zählvariable für Indizierung der Iterationsschritte
@@ -29,7 +30,7 @@ while max(c) > 0:
 
     # Abgrenzung der einzelnen Iterationsschritte in der Ausgabe
     print("")
-    print("Iterationsschritt:", k, "--------------------------")
+    print("Iterationsschritt :", k, "--------------------------")
     k = k + 1
 
     # PIVOT-SPALTE:-----------------------------------------------------------------------
@@ -41,53 +42,47 @@ while max(c) > 0:
     for i in range(len(A)):
         # Hilfsgröße q
         q[i] = b[i] / A[i, piv_s]
-    print("Quotient q:")
-    print(q)
+    print("Hilfsquotient \t q:", q)
 
     # Finde Indizes des kleinsten positiven Koeffizienten in q
     piv_r = np.argmin(q[q > 0.0])
 
     # PIVOT-Element:---------------------------------------------------------------------
     piv = A[piv_r, piv_s]
-    print("Pivot-Element [zeile, spalte]:")
-    print("A[", piv_r, ",", piv_s, "] =", piv)
+    print("Pivot-Element \t  :", "A[", piv_r, ",", piv_s, "] =", piv)
 
-    # Normieren der Pivot-Zeile von A
-    for j in range(len(A[0])):
-        A[piv_r, j] = A[piv_r, j] / A[piv_r, piv_s]
-
+    # Normieren der Pivot-Zeile von b und A
     b[piv_r] = b[piv_r] / A[piv_r, piv_s]
+    A[piv_r] = A[piv_r] / A[piv_r, piv_s]
 
     # ÄNDERUNG b-----------------------------------------------------------------------
     # Schleife über alle Zeilen von b
     for i in range(len(A)):
         if i != piv_r:
             b[i] = b[i] - b[piv_r] * A[i, piv_s]
-    print("Ergebnisvektor b:")
-    print(b)
+    print("Ergebnisvektor \t b:", b)
 
     # ÄNDERUNG z
     z = z - b[piv_r] * c[piv_s]
-    print("Gewinn:")
-    print(z)
+    print("Gewinn \t\t\t z:", z)
 
     # ÄNDERUNG c-----------------------------------------------------------------------
-    for j in range(len(A[0])):
-        for i in range(len(A)):
-            c[j] = c[j] - A[i, piv_s] * A[piv_r, j]
-    print("Zielfunktion:")
-    print(c)
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if j != piv_s:
+                c[j] = c[j] - A[i, piv_s] * A[piv_r, j]
+    c[piv_s] = 0
+    print("Zielfunktion \t c:", c)
 
     # ÄNDERUNG A-----------------------------------------------------------------------
-    for j in range(len(A[0])):                      # über Spalte j (0 bis 4)
-        for i in range(len(A)):                     # über Zeile i (0 bis 2)
+    for i in range(len(A)):
+        for j in range(len(A[0])):
             if i != piv_r and j != piv_s:
                 A[i, j] = A[i, j] - A[i, piv_s] * A[piv_r, j]
 
-    for j in range(len(A[0])):  # über Spalte j (0 bis 4)
-        for i in range(len(A)):  # über Zeile i (0 bis 2)
+    for i in range(len(A)):
+        for j in range(len(A[0])):
             if i != piv_r and j == piv_s:
                 A[i, j] = 0
     print("Koeffizientenmatrix A:")
     print(A)
-
