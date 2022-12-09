1e9215880d1edc7a1f336a81fa96eecd1e92bf0f
SirJowi <johann-sinner@web.de>
2022-12-09 16:59:16 +0100

    änderung zu indizes, vorherige normierung, anpassung der gleichungen

--- simplex.py
+++ simplex.py
@@ -4,19 +4,21 @@ import numpy as np
 
 # Koeffizienten des Gleichungssystems
 A = np.array([
-    [1, 1, -1, 0, 0],
-    [-1, 1, 0, 1, 0],
-    [-2, 1, 0, 0, -1]
+    [1.0, 1.0, -1.0, 0.0, 0.0],
+    [-1.0, 1.0, 0.0, 1.0, 0.0],
+    [-2.0, 1.0, 0.0, 0.0, -1.0]
 ])
 
 # Ergebnisvektor des Gleichungssystems
-b = np.array([5, 5, 2])
+b = np.array([5.0, 5.0, 2.0])
 
 # Koeffizienten der Zielfunktion
-c = np.array([2, 3, 0, 0, 0])
+c = np.array([2.0, 3.0, 0.0, 0.0, 0.0])
 
 # Platzhalter für Hilfsgröße q (Liste mit Nullen und der gleichen Zeilenanzahl wie A)
 q = np.empty(len(A))
+
+z = 0.0
 
 # Zählvariable für Indizierung der Iterationsschritte
 k = 1
@@ -32,54 +34,60 @@ while max(c) > 0:
 
     # PIVOT-SPALTE:-----------------------------------------------------------------------
     # Finde Indizes des größten Koeffizienten in der Zielfunktion c
-    piv_j = np.argmax(c)
+    piv_s = np.argmax(c)
 
     # PIVOT-ZEILE:-----------------------------------------------------------------------
     # Schleife über alle Zeilen von A
     for i in range(len(A)):
         # Hilfsgröße q
-        q[i] = b[i] / A[i, piv_j]
-
+        q[i] = b[i] / A[i, piv_s]
     print("Quotient q:")
     print(q)
 
     # Finde Indizes des kleinsten positiven Koeffizienten in q
-    piv_i = np.argmin(q[q > 0])
+    piv_r = np.argmin(q[q > 0.0])
 
     # PIVOT-Element:---------------------------------------------------------------------
-    piv = A[piv_i, piv_j]
-
+    piv = A[piv_r, piv_s]
     print("Pivot-Element [zeile, spalte]:")
-    print("A[", piv_i, ",", piv_j, "] =", piv)
+    print("A[", piv_r, ",", piv_s, "] =", piv)
+
+    # Normieren der Pivot-Zeile von A
+    for j in range(len(A[0])):
+        A[piv_r, j] = A[piv_r, j] / A[piv_r, piv_s]
+
+    b[piv_r] = b[piv_r] / A[piv_r, piv_s]
 
     # ÄNDERUNG b-----------------------------------------------------------------------
     # Schleife über alle Zeilen von b
     for i in range(len(A)):
-        # Normierung der Zeile i vom Ergebnisvektor b
-        if i == piv_i:
-            b[i] = b[i] / piv
-        # Berechnungen der übrigen Koeffizienten von b
-        else:
-            b[i] = b[i] - b[piv_i] * A[i, piv_j]
+        if i != piv_r:
+            b[i] = b[i] - b[piv_r] * A[i, piv_s]
     print("Ergebnisvektor b:")
     print(b)
 
+    # ÄNDERUNG z
+    z = z - b[piv_r] * c[piv_s]
+    print("Gewinn:")
+    print(z)
+
+    # ÄNDERUNG c-----------------------------------------------------------------------
+    for j in range(len(A[0])):
+        for i in range(len(A)):
+            c[j] = c[j] - A[i, piv_s] * A[piv_r, j]
+    print("Zielfunktion:")
+    print(c)
+
     # ÄNDERUNG A-----------------------------------------------------------------------
-    for s in range(len(A[0])):                      # über Spalte s (0 bis 4)
-        for r in range(len(A)):                     # über Zeile r (0 bis 2)
-            if s == piv_j and r == piv_i:
-                A[r, s] = 1 / piv
-            if s != piv_j and r == piv_i:
-                A[r, s] = A[r, s] / piv
-            if s == piv_j and r != piv_i:
-                A[r, s] = A[r, piv_j] - (A[r, piv_j] * A[piv_i, s]) / piv
-            if s != piv_j and r != piv_i:
-                A[r, s] = A[r, s] - (A[r, piv_j] * A[piv_i, s]) / piv
+    for j in range(len(A[0])):                      # über Spalte j (0 bis 4)
+        for i in range(len(A)):                     # über Zeile i (0 bis 2)
+            if i != piv_r and j != piv_s:
+                A[i, j] = A[i, j] - A[i, piv_s] * A[piv_r, j]
+
+    for j in range(len(A[0])):  # über Spalte j (0 bis 4)
+        for i in range(len(A)):  # über Zeile i (0 bis 2)
+            if i != piv_r and j == piv_s:
+                A[i, j] = 0
     print("Koeffizientenmatrix A:")
     print(A)
 
-    # ÄNDERUNG c-----------------------------------------------------------------------
-    for s in range(len(A[0])):
-        c[s] = c[s] - (c[piv_j] * A[piv_i, s]) / piv
-    print("Zielfunktion:")
-    print(c)
