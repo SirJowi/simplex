b4d6529c026a1fab9c912b1f93dc4097639d0a50
SirJowi <johann-sinner@web.de>
2022-12-10 15:19:02 +0100

    vereinfachung in c und A Kommentare

--- simplex.py
+++ simplex.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+# EINGABE DES TABLEAUS---------------------------------------------------------------
 # Koeffizienten des Gleichungssystems
 A = np.array([
     [1.0, 1.0, -1.0, 0.0, 0.0],
@@ -15,6 +16,7 @@ b = np.array([5.0, 5.0, 2.0])
 # Koeffizienten der Zielfunktion
 c = np.array([2.0, 3.0, 0.0, 0.0, 0.0])
 
+# SIMPLEX-ALGORITHMUS beginnt hier---------------------------------------------------
 # Platzhalter für Hilfsgröße q (Liste mit Nullen und der gleichen Zeilenanzahl wie A)
 q = np.empty(len(A))
 
@@ -24,9 +26,9 @@ z = 0.0
 # Zählvariable für Indizierung der Iterationsschritte
 k = 1
 
-# Abbruchkriterium-------------------------------------------------------------------------
+# Abbruchkriterium-------------------------------------------------------------------
 # Das Ergebnis lässt sich verbessern, solange Koeffizienten der Zielfunktion > 0
-while max(c) > 0 and k < 4:
+while max(c) > 0 and k < 4:     # k begrenzt, damit Schleife nicht unendlich läuft
 
     # Abgrenzung der einzelnen Iterationsschritte in der Ausgabe
     print("")
@@ -34,35 +36,33 @@ while max(c) > 0 and k < 4:
     print("Iterationsschritt :", k,)
     k = k + 1
 
-    # PIVOT-SPALTE:-----------------------------------------------------------------------
+    # PIVOT-SPALTE:------------------------------------------------------------------
     # Finde Indizes des größten Koeffizienten in der Zielfunktion c
-    piv_s = np.argmax(c)
+    piv_s = np.argmax(c)    # liefert die Position des größten Wertes in Array
 
-    # PIVOT-ZEILE:-----------------------------------------------------------------------
+    # PIVOT-ZEILE:-------------------------------------------------------------------
     # Schleife über alle Zeilen von A
     for i in range(len(A)):
-        # Hilfsgröße q
-        q[i] = b[i] / A[i, piv_s]
+        q[i] = b[i] / A[i, piv_s]   # Hilfsgröße q
     print("Hilfsquotient \t q:", q)
 
     # Finde Indizes des kleinsten positiven Koeffizienten in q
     for i in range(len(q)):
         if q[i] < 0:
-            q[i] = np.inf
-    piv_r = np.argmin(q)
+            q[i] = np.inf   # setzt alle Einträge < 0 auf Unendlich
+    piv_r = np.argmin(q)    # liefert die Position des kleinsten Wertes in Array
 
-    # PIVOT-Element:---------------------------------------------------------------------
+    # PIVOT-Element:-----------------------------------------------------------------
     piv = A[piv_r, piv_s]
     print("Pivot-Element \t  :", "A[", piv_r, ",", piv_s, "] =", piv)
 
-    # Normieren der Pivot-Zeile von b und A
+    # Normieren der Pivot-Zeile von b und A------------------------------------------
     b[piv_r] = b[piv_r] / A[piv_r, piv_s]
-    A[piv_r] = A[piv_r] / A[piv_r, piv_s]
+    A[piv_r] = A[piv_r] / A[piv_r, piv_s]   # ganze Zeile von A durch Pivot-Element
 
-    # ÄNDERUNG b-----------------------------------------------------------------------
-    # Schleife über alle Zeilen von b
-    for i in range(len(b)):
-        if i != piv_r:
+    # ÄNDERUNG b---------------------------------------------------------------------
+    for i in range(len(b)):     # Schleife über alle Zeilen von b
+        if i != piv_r:      # alle Elemente von b außer Pivot-Zeile
             b[i] = b[i] - b[piv_r] * A[i, piv_s]
     print("Ergebnisvektor \t b:", b)
 
@@ -70,26 +70,22 @@ while max(c) > 0 and k < 4:
     z = z - b[piv_r] * c[piv_s]
     print("Gewinn \t\t\t z:", z)
 
-    # ÄNDERUNG c-----------------------------------------------------------------------
+    # ÄNDERUNG c---------------------------------------------------------------------
     for j in range(len(A[0])):
-        if j != piv_s:
+        if j != piv_s:      # alle Elemente von c außer Pivot-Spalte
             c[j] = c[j] - c[piv_s] * A[piv_r, j]
 
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            if i != piv_r and j == piv_s:
-                c[j] = 0
+    c[piv_s] = 0            # Pivot-Spalte erst zum Schluss 0 setzen
     print("Zielfunktion \t c:", c)
 
-    # ÄNDERUNG A-----------------------------------------------------------------------
+    # ÄNDERUNG A---------------------------------------------------------------------
     for i in range(len(A)):
         for j in range(len(A[0])):
-            if i != piv_r and j != piv_s:
+            if i != piv_r and j != piv_s:   # alle Elemente von A außer Pivot-Element
                 A[i, j] = A[i, j] - A[i, piv_s] * A[piv_r, j]
 
     for i in range(len(A)):
-        for j in range(len(A[0])):
-            if i != piv_r and j == piv_s:
-                A[i, j] = 0
+        if i != piv_r:
+            A[i, piv_s] = 0     # Pivot-Spalte erst zum Schluss 0 setzen
     print("Koeffizientenmatrix A:")
     print(A)
